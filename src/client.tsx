3a6ec732327ca7cb40cc92cd419c2913f2863d4b
tom <tombousso@gmail.com>
2016-05-21 18:05:40 -0700

    Math

--- src/client.tsx
+++ src/client.tsx
@@ -3,6 +3,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as socket from "socket.io-client";
 
+var math = require("mathjs");
+math.config({
+  number: 'Fraction'
+});
+
 var io = socket.connect(window.location.href);
 
 ReactDOM.render(
