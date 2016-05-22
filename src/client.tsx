751c0dc38dc63a9ebe4cb2cca9fa6f66437bdce7
Max Ovsiankin <max@ovsankin.com>
2016-05-21 17:28:56 -0700

    wow

--- src/client.tsx
+++ src/client.tsx
@@ -5,9 +5,16 @@ import * as socket from "socket.io-client";
 
 var io = socket.connect(window.location.href);
 
+class Hello extends React.Component<any, {}> {
+  wow(event) {
+    alert(this.props.to);
+  }
+  render() {
+    return <h1 onClick={(evt) => this.wow(evt)}>Hello, {this.props.to}!</h1>;
+  }
+}
+
 ReactDOM.render(
-  <h1>
-    Hey!
-  </h1>,
+  <Hello to="Tom"/>,
   document.getElementById("container")
 );
